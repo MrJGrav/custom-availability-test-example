d2c0e460fa5bebcd7b548f65303fa9678798dbae
Tom Meadon <tom@meadon.me>
2021-06-25 11:22:53 +0100

    add some comments

--- FunctionApp/RunAvailabilityTest/run.ps1
+++ FunctionApp/RunAvailabilityTest/run.ps1
@@ -2,18 +2,24 @@
 param($Timer)
 
 $webAppUrl = "https://$($env:webAppHostname)"
+
+# use Invoke-WebRequest to send a request to the web app's URL and store the result so we can check the status code
+# also, track the start and end times for the request so we can calculate the duration later on
 $startTime = Get-Date
 $result = Invoke-WebRequest -Uri $webAppUrl -SkipHttpErrorCheck
 $endTime = Get-Date
 
+# if the status code is in the range of client errors or server errors then mark the test as failed
 if ($result.StatusCode -ge 400 -and $result.StatusCode -le 599)
 {
     $success = $false
     $message = "Web app responding with HTTP status code $($result.StatusCode)"
 }
 
+# create a new app insights client using the instrumentation key stored in the 'APPINSIGHTS_INSTRUMENTATIONKEY' app setting
 $appInsightsClient = New-AppInsightsClient -InstrumentationKey $env:APPINSIGHTS_INSTRUMENTATIONKEY
 
+# send the availability test result back to app insights
 $appInsightsParams = @{
     AppInsightsClient = $appInsightsClient
     TestName          = $webAppUrl
